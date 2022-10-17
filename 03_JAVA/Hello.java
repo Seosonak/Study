176cd1dd60fa6b7707ca3fb023a30d440cce1b16
Seosonak <nakoa346@gmail.com>
2022-10-17 17:31:28 +0900

    up

---
+++ 03_JAVA/Hello.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Hello {
+    public static void main(String[] args) {
+        
+        String coco = "cat";
+        String cat = "cat";
+        String dog = "dog";
+        boolean cocoIsCat = true;
+        boolean cocoIsDog = false;
+        if(BomIsCat){ //false
+        System.out.println("봄이는 강아지입니다.");
+    } else if(!BomIsCat){ //true
+        System.out.println("봄이는 고양이가 아닙니다");
+        System.out.println("봄이는 강아지입니다.");
+    } 
+                        // System.out.println("Hello World"); //
+        // int a;
+        // int b;
+        // a=1;
+        // b=2;
+        // System.out.println(a+b);
+    }
+}
